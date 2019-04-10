706c1e347a975db2aac11f454940255c7aa8f4a1
Pradeep Kumar <pradeepk.pdp@gmail.com>
2019-04-10 23:47:02 +0530

    clean up

--- tests/google-search.js
+++ tests/google-search.js
@@ -4,14 +4,6 @@ const { openBrowser,write, closeBrowser, goto, click } = require('taiko');
 const assert = require("assert");
 const headless = process.env.headless_chrome.toLowerCase() === 'true';
 
-// beforeSuite(async () => {
-//     await openBrowser({ headless: headless })
-// });
-
-// afterSuite(async () => {
-//     await closeBrowser();
-// });
-
 step("Open google.com", async () => {
     await goto("https://google.com");
 });
